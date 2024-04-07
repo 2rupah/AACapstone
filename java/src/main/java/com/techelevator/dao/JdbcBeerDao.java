44990a052b83a2be5ea1287f54e1cfab6f591723
Jamea Tuck <jameatuck@gmail.com>
2024-04-07 19:28:24 -0400

    Created endpoints

--- java/src/main/java/com/techelevator/dao/JdbcBeerDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcBeerDao.java
@@ -3,11 +3,13 @@ package com.techelevator.dao;
 import com.techelevator.model.Beer;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class JdbcBeerDao implements BeerDao {
 
     private JdbcTemplate jdbcTemplate;
@@ -19,7 +21,7 @@ public class JdbcBeerDao implements BeerDao {
     @Override
     public Beer getBeerById(int beerId) {
         Beer beer = null;
-        String sql = "SELECT beer_id " +
+        String sql = "SELECT * " +
                 "FROM beer " +
                 "WHERE beer_id = ?;";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, beerId);
@@ -32,7 +34,7 @@ public class JdbcBeerDao implements BeerDao {
     @Override
     public List<Beer> getAllBeersFromBrewery(int breweryId) {
         List<Beer> beers = new ArrayList<>();
-        String sql = "SELECT beer_id, brewery_id " +
+        String sql = "SELECT * " +
                 "FROM beer " +
                 "WHERE brewery_id = ?;";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, breweryId);
