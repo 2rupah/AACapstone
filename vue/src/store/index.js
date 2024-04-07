ea67a26bf0ce504fd2d3025ca7fa5097b9f806a4
Alex Charnes <acharnes7817@gmail.com>
2024-04-07 18:48:27 -0400

    Capstone 4/7/24 push

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,13 +1,26 @@
 import { createStore as _createStore } from 'vuex';
 import axios from 'axios';
+import BreweryService from '../services/BreweryService'
 
 export function createStore(currentToken, currentUser) {
   let store = _createStore({
     state: {
       token: currentToken || '',
-      user: currentUser || {}
+      user: currentUser || {},
+      breweryList: []
+    },
+    actions: {
+      getAllBreweries(context) {
+        BreweryService.getAllBreweries().then(response => {
+          context.commit('SET_BREWERYLIST', response)
+        })
+      }
+
     },
     mutations: {
+      SET_BREWERYLIST(state, brewery){
+        state.breweryList = brewery
+      },
       SET_AUTH_TOKEN(state, token) {
         state.token = token;
         localStorage.setItem('token', token);
