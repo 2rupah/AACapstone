f32e7575a90c4309085164514a395f9bdaad54fc
Alex Charnes <acharnes7817@gmail.com>
2024-04-09 12:39:35 -0400

    pull

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,10 +1,43 @@
 <template>
-  <div id="capstone-app">
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }" class="btn btn-primary mr-2">Home</router-link>
-      <router-link v-bind:to="{ name: 'brewery' }" class="btn btn-primary mr-2">BreweryList</router-link>
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''" class="btn btn-primary">Logout</router-link>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="@/assets/logo.jpg" alt=""></a>
+      
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Brewery</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
     </div>
-    <router-view />
-  </div>
-</template>
+  </nav>
+    <!-- <div id="capstone-app">
+      <div id="nav">
+        <router-link v-bind:to="{ name: 'home' }" class="btn btn-primary mr-2">Home</router-link>
+        <router-link v-bind:to="{ name: 'brewery' }" class="btn btn-primary mr-2">BreweryList</router-link>
+        <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''" class="btn btn-primary">Logout</router-link>
+      </div>
+      <router-view />
+    </div> -->
+  </template>
+
+  <style scoped>
+    img {
+      height:auto;
+      width: 12%;
+    }
+  </style>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <template>
   
-  <div class="background"></div>  
+
   <div class="overlay"></div>
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    
 
 </template>
 
@@ -15,20 +14,24 @@ export default {
 <style scoped>
 
 
-.background {
-  background-color: #0D1822;
-  height: 100vh;
+.overlay {
+  position: relative;
   width: 100vw;
+  height: 100vh; /* Adjust this according to your needs */
+  background-color: #0A1823;
  
 }
-.overlay {
-    height: 100vh;
-    width: 100vw;
-    position: relative;
-    background-size: contain;
-    background-position:center;
-    background-repeat: no-repeat;
-
-    background-image: url('../assets/Logo.jpg'); 
-  }
+.overlay::before {
+  content: "";
+  position:fixed;
+  width: 100%;
+  height: 100%;
+  background-image: url('../assets/Logo.jpg'); /* Path to your image */
+  background-size: contain;
+  opacity: 0.5; /* Adjust the opacity as needed */
+  background-position: center;
+  background-repeat: no-repeat;
+  
+}
 </style>
+
