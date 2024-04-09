8d9b94ac662c306dbc11c33279f41adbeb88a6a4
Zach Moretti <Zachmoretti11@gmail.com>
2024-04-09 11:00:35 -0400

    push

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,11 +1,34 @@
 <template>
-  <div class="home">
+  
+  <div class="background"></div>  
+  <div class="overlay"></div>
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
-  </div>
+
 </template>
 
 <script>
 export default {
 };
 </script>
+
+<style scoped>
+
+
+.background {
+  background-color: #0D1822;
+  height: 100vh;
+  width: 100vw;
+ 
+}
+.overlay {
+    height: 100vh;
+    width: 100vw;
+    position: relative;
+    background-size: contain;
+    background-position:center;
+    background-repeat: no-repeat;
+
+    background-image: url('../assets/Logo.jpg'); 
+  }
+</style>
