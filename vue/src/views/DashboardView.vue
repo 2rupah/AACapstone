93a32871c474e93895107f156fcde20726929eb3
Alex Charnes <acharnes7817@gmail.com>
2024-04-11 11:40:24 -0400

    push

--- vue/src/views/DashboardView.vue
+++ vue/src/views/DashboardView.vue
@@ -1,22 +1,22 @@
 <template>
-
-
+    <h1>Hi</h1>
 </template>
 
 <script>
 import BreweryService from '../services/BreweryService';
 
+export default {
 
+    deleteBeer(beerId) {
+        const breweryIdFromUrlParams = this.$route.params.id;
+        BreweryService.getBeersByBreweryId(breweryIdFromUrlParams)
+            .then(response => {
+                this.beerList = response.data;
 
-  deleteBeer(beerId) {
-    const breweryIdFromUrlParams = this.$route.params.id;
-    BreweryService.getBeersByBreweryId(breweryIdFromUrlParams)
-      .then(response => {
-        this.beerList = response.data;
-        
-      })
-      
-      .catch(err => console.error(err));
+            })
 
-  }
+            .catch(err => console.error(err));
+
+    }
+}
 </script>
