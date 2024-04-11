775394c0ac368e71299a41f33b1f7850bffc40c8
Alex Charnes <acharnes7817@gmail.com>
2024-04-11 11:33:17 -0400

    add/remove beer

---
+++ vue/src/components/AddBeer.vue
@@ -0,0 +1,37 @@
+<template>
+
+
+</template>
+
+<script>
+import BreweryService from '../services/BreweryService';
+
+
+export default {
+    props: ['beerId'],
+    data() {
+        return {
+            newBeer: {}
+        };
+    },
+
+methods: {
+    addBeer() {
+    this.newBeer.beerId = this.beerId;
+    BreweryService.addBeer(this.newBeer, this.beerId)
+      .then(response => {
+        if (response.status === 201 || response.status === 200) {
+            this.$router.push( { 
+              name: 'login-dashboard' } );
+            }
+      })
+      
+      .catch(err => console.error(err));
+
+  },
+  resetForm() {
+    this.newBeer = {}
+  }
+}
+}
+</script>
