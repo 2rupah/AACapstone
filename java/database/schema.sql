9906cf60eb9abb793495db19f1e49a5ae72ada3e
Andrew Woronka <drupah824@gmail.com>
2024-04-15 12:36:50 -0400

    4.15.2024 - grabbing stuff once more

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,31 @@
 BEGIN TRANSACTION;
 
+DROP TABLE IF EXISTS user_brewery;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS beer;
+DROP TABLE IF EXISTS brewery;
+
+CREATE TABLE brewery (
+    brewery_id serial PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    location VARCHAR(150) NOT NULL,
+    established_year INT,
+    description TEXT,
+    imageURL VARCHAR(255)
+);
+
+CREATE TABLE beer (
+    beer_id serial PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    brewery_id INT,
+    style VARCHAR(100),
+    abv DECIMAL(4, 2),
+    ibu INT,
+    description TEXT,
+    FOREIGN KEY (brewery_id) REFERENCES brewery(brewery_id),
+    imageURL VARCHAR(255),
+	rating INT
+);
 
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
@@ -18,28 +43,6 @@ CREATE TABLE user_brewery (
     CONSTRAINT FK_user FOREIGN KEY (user_id) REFERENCES users(user_id),
     CONSTRAINT FK_brewery FOREIGN KEY (brewery_id) REFERENCES brewery(brewery_id),
     PRIMARY KEY (user_id, brewery_id)
-);
-
-
-CREATE TABLE brewery (
-    brewery_id serial PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    location VARCHAR(150) NOT NULL,
-    established_year INT,
-    description TEXT,
-    imageURL VARCHAR(255) 
-);
-
-CREATE TABLE beer (
-    beer_id serial PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    brewery_id INT,
-    style VARCHAR(100),
-    abv DECIMAL(4, 2),
-    ibu INT,
-    description TEXT,
-    FOREIGN KEY (brewery_id) REFERENCES brewery(brewery_id),
-    imageURL VARCHAR(255) 
 );
 
 COMMIT TRANSACTION;
