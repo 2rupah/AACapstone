772fc43342325c98adb090e2470a1fb6598cfb0c
Alex Charnes <acharnes7817@gmail.com>
2024-04-18 09:36:57 -0400

    Map and DB update

--- vue/src/views/PubCrawlView.vue
+++ vue/src/views/PubCrawlView.vue
@@ -1,21 +1,42 @@
 <template>
     <div id="layout">
-        <Map />
+        <Map :breweryMarker="breweryMarker"/>
     </div>
     <div id="breweryList">
         
     </div>
+    <MapBreweryList :brewery="brewery" @addBrewery="addBreweryToMap"/>
 </template>
 
 <script>
 import Map from '../components/Map.vue';
+import MapBreweryList from '../components/MapBreweryList.vue';
+import BreweryService from '../services/BreweryService';
 // import "/node_modules/mapbox-gl/dist/mapbox-gl.css"
 
 
 export default {
     components: {
         Map,
+        MapBreweryList
         
+    },
+    methods: {
+        addBreweryToMap(breweryId) {
+            BreweryService.getBreweryInfo(breweryId)
+            .then(response => {
+          this.breweryMarker = response.data;
+          console.log(this.breweryMarker)
+        })
+        .catch(error => {
+          console.error('Error fetching brewery information:', error);
+        });
+        }
+    },
+    data() {
+        return {
+            breweryMarker: {}
+        }
     }
 }
 
